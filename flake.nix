ad02ebd3ccd3d4055fbd7d5b7030f55940a01380
Suchir Kavi <suchirkavi@gmail.com>
2026-05-01 16:18:30 -0700

    scaffolding

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "gridlock";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          formatter = pkgs.nixfmt-tree;
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              bazel_9
+              buildifier
+            ];
+          };
+        };
+    };
+}
